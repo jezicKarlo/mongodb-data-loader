20d01bc3fdd46f662c21d41d7de3ba6ee5ac4975
jezicKarlo <karlo.jezic@gmail.com>
2020-12-28 13:55:46 +0100

    implement reader

---
+++ src/main/java/hr/fer/advanced_databases/reader/DataReader.java
@@ -0,0 +1,24 @@
+package hr.fer.advanced_databases.reader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DataReader implements AutoCloseable {
+
+    private final BufferedReader reader;
+
+    public DataReader(String filePath) throws FileNotFoundException {
+        reader = new BufferedReader(new FileReader(filePath));
+    }
+
+    public String readItem() throws IOException {
+        return reader.readLine();
+    }
+
+    @Override
+    public void close() throws Exception {
+        reader.close();
+    }
+}
--- src/main/java/hr/fer/advanced_databases/reader/FileReader.java
+++
@@ -1,4 +0,0 @@
-package hr.fer.advanced_databases.reader;
-
-public class FileReader {
-}
