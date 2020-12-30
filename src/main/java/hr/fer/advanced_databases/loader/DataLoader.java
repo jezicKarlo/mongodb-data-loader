b4a6da28064c01a569a216bc8c17b0e6de9561ef
jezicKarlo <karlo.jezic@gmail.com>
2020-12-30 19:23:12 +0100

    start of dataloader

--- src/main/java/hr/fer/advanced_databases/loader/DataLoader.java
+++ src/main/java/hr/fer/advanced_databases/loader/DataLoader.java
@@ -1,6 +1,17 @@
 package hr.fer.advanced_databases.loader;
 
-public class DataLoader {
+import hr.fer.advanced_databases.converter.DataConverter;
+import hr.fer.advanced_databases.parser.DataParser;
 
+public abstract class DataLoader<T> {
 
+    private DataConverter<T> converter;
+    private DataParser parser;
+
+    public DataLoader(DataConverter converter, DataParser parser) {
+        this.converter = converter;
+        this.parser = parser;
+    }
+
+    public abstract void loadData();
 }
